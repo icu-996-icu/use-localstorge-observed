e646b9125e540ad81a7e468dc98081c8df698d63
九月 <68600219+icu-996-icu@users.noreply.github.com>
2021-03-31 14:02:50 +0800

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
 import useLocalStorageObserved from './'
 import { renderHook } from "@testing-library/react-hooks";
 
+// mock timer using jest
+jest.useFakeTimers();
 
 describe('useLocalStorageObserved', () => {
   it('updates every second', () => {
-    const { result } = renderHook(() => useLocalStorageObserved('token'));
+    const { result } = renderHook(() => useLocalStorageObserved({keyName:'token'}));
 
     expect(result.current).toBe(false);
 
